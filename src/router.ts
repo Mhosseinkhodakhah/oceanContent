1794a18cf799c166fb5524ff24ae5a350d3e4f4f
hossein <kianlucifer0098@gmail.com>
2024-11-30 12:01:02 +0330

    change to set content service

--- src/router.ts
+++ src/router.ts
@@ -9,20 +9,7 @@ const auth = new middleWare().auth
 const router = Router()
 
 
-router.get('/get-lessons/:lang' , controller.getLessons)
-
-router.get('/get-sublesson/:contentId/:lang' , controller.getSubLesson)
-
-router.get('/get-content/:contentId/:lang' , controller.getContent)
-
 router.put('/seen-content/:contentId' , auth , controller.seenContent)
 
-router.get('/get-levels' , auth , controller.getLevels)
-
-router.get('/open-level/:number' , auth , controller.openLevel)
-
-router.put('/answer-question' , auth , controller.answer)
-
-router.get('/get-contents' , auth , controller.getAllContent)
 
 export default router;
