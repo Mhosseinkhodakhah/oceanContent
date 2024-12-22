e844124ec90db4339334431d98c338adba2781ac
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2024-12-22 15:55:19 +0330

    asdf

--- src/interservice/connection.ts
+++ src/interservice/connection.ts
@@ -20,15 +20,34 @@ export default class interConnection {
     }
 
     async resetCache() {
-        const rawResponse = await fetch(`http://localhost:5003/app/interservice/reset-cache`, {
+        const rawResponse = await fetch(`http://localhost:5003/app/interservice/reset-cache`, {      //get content
             method: 'PUT',
             headers: {
                 Accept: "*/*",
                 "Content-Type": "application/json",
             },
-        }) 
+        })
+
+        const rawResponse2 = await fetch(`http://localhost:5005/app/interservice/reset-cache`, {   // get user
+            method: 'PUT',
+            headers: {
+                Accept: "*/*",
+                "Content-Type": "application/json",
+            },
+        })
+
+        const rawResponse3 = await fetch(`http://localhost:5004/app/interservice/reset-cache`, {   // get quize
+            method: 'PUT',
+            headers: {
+                Accept: "*/*",
+                "Content-Type": "application/json",
+            },
+        })
+
 
         const response = await rawResponse.json()
+        const response2 = await rawResponse2.json()
+        const response3 = await rawResponse3.json()
         return response;
     }
 
